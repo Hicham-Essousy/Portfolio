e3a57bcdedc3a8974e24d32ec7095d0d2acc8862
unknown <hicham.es-sousy@edu.uiz.ac.ma>
2021-02-10 15:39:23 +0100

    adding portfolio to my github

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //toggle navbar
+    $('.menu-btn').click(function(){
+        $('.nav-links').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
